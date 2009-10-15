afddde089f90e90ef3a26ef5a14fa6a1f39ca64c
phillc <pccampbell@patrickdavis.com>
2009-10-15 11:20:25 -0400

    Made a bunch of changes to fit my needs

--- src/state_machine.js
+++ src/state_machine.js
@@ -1,19 +1,29 @@
-var undefinedState = "State is undefined";
-var undefinedEvent = "Event is undefined";
-
 function StateMachine(){
-    this.stateMachine = true;
-    this.state = false;
+    this.stateMachine = true; // for debug, state machine is attached
+    this.state = false; // no state set yet
 };
-StateMachine.prototype.handleEvent = function(event){
-    if(!this.state[event]) throw undefinedEvent;
-    this.state[event].call(this);
-}
-StateMachine.prototype.changeState = function(state){
-    if(!this.states[state]) return;
-    if(this.state){
-        this.handleEvent('exit');
+StateMachine.prototype.handleEvent = function(eventName){
+    // if the before event function returns false, stop (return false)
+    // if the function exists, AND teh function returns returns false, return false
+    if((typeof(this.beforeEvent) != 'undefined') && (this.beforeEvent.call(this, eventName) == false)) return false;
+    var event = this.states[this.state][eventName];
+    if(typeof(event) === undefined) return false;
+    if(typeof(event) === String) return this.changeState.apply(this, arguments); // Go to that state
+    if(typeof(event) == 'function'){
+        var result = event.apply(this, Array.prototype.slice.call(arguments, 1)); // Pass args to function
+        if(typeof(result) === String) return this.changeState.apply(this); // Go to that state
     }
-    this.state = this.states[state];
-    this.handleEvent('enter');
+    if(typeof(this.afterEvent) != 'undefined') this.afterEvent.call(this, eventName);
+    return this;
+}
+StateMachine.prototype.changeState = function(newState){
+    var oldState = this.state;
+    if((typeof(this.beforeStateChange) != 'undefined') && (this.beforeStateChange.call(this, newState, oldState) == false)) return false;
+    if(oldState == newState) return this; // already there, not going to change to it
+    if(!this.states[newState]) return false;
+    if(oldState) this.handleEvent('exit'); // silent, since im considering exit optional
+    this.state = newState;
+    this.handleEvent('enter'); // silent, since im considering enter optional
+    if(typeof(this.afterStateChange) != 'undefined') this.afterStateChange.call(this, newState, oldState);
+    return this;
 }
